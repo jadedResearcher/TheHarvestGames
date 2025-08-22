366d81f269697b0f75f994965aa47d9117debfca
Jennifer Reynolds <jadedResearcher@gmail.com>
2025-08-21 22:26:58 -0400

    hint

--- secrets/DEAD.js
+++ secrets/DEAD.js
@@ -1,5 +1,14 @@
-const doThing = () => {
+/*
+A relic can mutate the global name space (including the game)
+however it likes. 
+
+On refresh, a relic will no longer be in effect.
+
+A relic should add itself to localStorage so it can be rendered as a book
+you can click any time in order to re-apply it (along with the popup)
+*/
+const doDEAD = () => {
     alert("HOPY SHIT THIS WORKS")
 }
 
-doThing();
+doDEAD();
