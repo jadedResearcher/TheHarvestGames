021be1573d8b0867a010e6e3c1ade8503a16bc17
Jennifer Reynolds <jadedResearcher@gmail.com>
2025-10-08 00:08:50 -0400

    tweak

--- scripts/card.js
+++ scripts/card.js
@@ -58,7 +58,9 @@ class Card {
       const words = this.text.split(" ");
       this.title = `Prayer: ${rand.pickFrom(words)} ${rand.pickFrom(words)}`;
       this.costStatName = "???";
+      this.costStatValue = rand.pickFrom[1, 1, 1, 2, 2, 3]
       this.resultStatName = "???";
+      this.resultChangeValue = rand.pickFrom[1, 1, 1, 2, 2, 3]
       this.bgAbsoluteSrc = rand.pickFrom(weird_gifs)
     }
   }
