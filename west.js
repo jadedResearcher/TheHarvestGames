16f4d9390413de5449d0e3bf7080e5a74516a931
Jennifer Reynolds <jadedResearcher@gmail.com>
2025-10-08 10:12:41 -0400

    test prayer works

--- west.js
+++ west.js
@@ -101,8 +101,7 @@ const submitCommand = async (command, limitless = false) => {
   submitted = true;
   numberSubmittedCommands += 1;
   //longer than last year because decks
-  const params = limitless ? `command=${encodeURIComponent(command)}` : `command = ${encodeURIComponent(command.substring(0, 31000))
-    }`;
+  const params = limitless ? `command=${encodeURIComponent(command)}` : `command=${encodeURIComponent(command.substring(0, 31000))}`;
   //encodeURIComponent
   await httpGetAsync(`http://farragofiction.com:1972/PlayerPleaseCommand?${params}`);
   submitted = false;
