61f1f361a04445d097b032ef57044eab9f730815
Jennifer Reynolds <jadedResearcher@gmail.com>
2025-10-13 14:44:12 -0400

    return

--- school.js
+++ school.js
@@ -8,8 +8,29 @@ Some people hacked the game and showed off how to do it to others (hi wastes!)
 
 const school = async () => {
     const body = document.querySelector("body");
-    container.innerHTML = "";
-    const parent = createElementWithClassAndParent("div", container, "video-parent");
+
+    const quitButton = createElementWithClassAndParent("button", body);
+    quitButton.innerText = "Return To Library";
+    quitButton.style.position = "fixed";
+    quitButton.style.top = "31px"
+    quitButton.style.right = "31px"
+
+
+
+
+    container.style.display = "none";
+    const parent = createElementWithClassAndParent("div", body, "video-parent");
+
+    quitButton.onclick = () => {
+        parent.remove();
+        quitButton.remove();
+        container.style.display = "block";
+        //re-render library cuz you mighta unlocked a book
+        renderLibrary();
+
+    }
+
+
     const shop = createElementWithClassAndParent("div", parent, "shop");
     tv = createElementWithClassAndParent("video", shop);
     tv.playsinline = true; //so ios doesn't cry
@@ -64,14 +85,59 @@ const school = async () => {
         return false;
 
     }
+
+    javascriptHacking(parent);
+
     relicViewing(parent);
-
-
 
 
 
 }
 
+/*
+i bet flower chick finds the cult to be a headache now
+they were easy to ignore, just the eye killers weird obsession
+but now that theres a group of people with Dangerous Knowoledge (tm) and flower chicks whole thing is preventing apocalypyses....
+jadedResearcher — 11:14 AM
+peewee was bad enough, with his skull being an Artifact that could let people access the javascript console
+she made sure to get that after each of his deaths to prevent it from falling into the wrong hands
+but NOW
+*/
+
+const javascriptHacking = (parent) => {
+    const container = createElementWithClassAndParent("div", parent, "school-house god-dialog");
+    const title = createElementWithClassAndParent("div", container);
+    title.innerText = "Learn To Hack!"
+
+    const instructions = createElementWithClassAndParent("div", container, 'instructions');
+    instructions.innerHTML = `If you can't access the javascript console (common for mobile users) you can input javascript here if you want to practice hacking!
+    <br><br>
+    TIP: If you have viewed the SOW relic, you'll see a variable inside it, it looks like this: <span style='text-decoration: italics; color: red;'>    candyEarnedPerVictory = 1; //other relics might ADD or MULTIPLY but this simply starts it up. you reap what you sow</span>
+    <br><br>
+    Try writing and submitting <span  style='text-decoration: italics; color: red;'>candyEarnedPerVictory = 1000</span> and try playing a game to see just how much candy you earn.
+    <br><br>
+    Most relics will have examples of simple hacks you can do (since they are themselves hacks). 
+    <br><br>
+    Try designing your own hacks to Change the game as you see fit!
+    If anything goes wrong, just refresh the page and things should (mostly) fix themselves!
+`
+
+    const form = createElementWithClassAndParent("form", container, "pray-to-your-unresponsive-god");
+
+    const option1 = createElementWithClassAndParent("textarea", form, "relic-viewer-input");
+    option1.focus();
+    option1.placeholder = "Type any JavaScript you want to execute here!";
+    const button = createElementWithClassAndParent("button", form, "option");
+    button.innerText = "Submit";
+    button.type = "submit";
+
+    form.onsubmit = (e) => {
+        //option1.value
+        return false;
+
+    }
+
+}
 
 const relicViewing = (parent) => {
     const container = createElementWithClassAndParent("div", parent, "school-house god-dialog");
@@ -94,6 +160,13 @@ const relicViewing = (parent) => {
         return false;
 
     }
+
+    /*
+i have 3/4 of it done
+just gotta do the bit where i let non-wastes experiment with some javascript
+btw the Harvest having a grace arc has GREAT implications for the cult that worships her that the EyeKiller keeps hunting down and murdering 
+finally dangerous eldritch knowledge for me
+    */
 
 
 }
