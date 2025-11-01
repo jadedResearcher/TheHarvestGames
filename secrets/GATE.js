a85b5bc025ae45b19bf8e2ae6876d21fb6d87896
Jennifer Reynolds <jadedResearcher@gmail.com>
2025-11-01 00:07:41 -0400

    she sleeps

---
+++ secrets/GATE.js
@@ -0,0 +1,8 @@
+
+const doTRUTH = () => {
+    truthLog("GATE", "Gambling/Gardening, Arbitration, Teaching, Eating... In Truth it only occured to JR at the last minute to have the four motivations spell something like a RELIC would from stats. It had to be an anagram, of course, because the True order is GTEA which is not as interesting. God-Tea perhaps? No matter. What does matter is that JR plans to write an epilogue on just how this newly minted adult god will effect Zampanio.")
+}
+
+doTRUTH();
+
+
